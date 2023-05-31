1f3a073965386e4c61efb6c81fa4123dba3f0e0d
LucasM548 <lucasm54800@gmail.com>
2023-05-31 19:56:06 +0200

    ga

--- js/script.js
+++ js/script.js
@@ -98,18 +98,9 @@ $(document).ready(function() {
 /**======================
  *    GOOGLE ANALYTICS
  *========================**/
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://raw.githubusercontent.com/LucasM548/G-A/main/An-s.js','ga');
-
-ga('create', 'G-JDZE4D8YXC', 'auto');
-ga('send', 'pageview');
-
-/*
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-JDZE4D8YXC');*/
+gtag('config', 'G-JDZE4D8YXC');
 /*==== END OF GOOGLE ANALYTICS ====*/
