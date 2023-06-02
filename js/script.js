29b57976222073f5754c7662c20df9b09c750ef5
LucasM548 <lucasm54800@gmail.com>
2023-06-02 07:28:08 +0200

    d

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ gtag('config', 'G-JDZE4D8YXC');
 
 (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "hdfskdqo11");
+})(window, document, "clarity", "script", "hdfqskztf2");
 /*==== END OF ANALYTICS ====*/
